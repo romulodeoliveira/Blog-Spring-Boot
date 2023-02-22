b5ef16aca955ec11b0d3aefaec8dc7e7816d45e8
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-02-22 18:33:58 -0300

    criação do model para postagens

---
+++ src/main/java/com/api/blog/models/Postagem.java
@@ -0,0 +1,45 @@
+package com.api.blog.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "postagens")
+public class Postagem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+
+    private String body;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+}
